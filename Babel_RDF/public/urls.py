72f5ca83c91762ee37e6576764922c1c790f9850
Moomooo95 <mohamed.ali.benzina.pro@gmail.com>
2022-02-11 14:17:44 +0100

    + add image if no images availlable + add manga list

--- Babel_RDF/public/urls.py
+++ Babel_RDF/public/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('mangas', views.manga_list, name='manga_list'),
+    path('mangas/page/<int:page>', views.manga_list, name='manga_list'),
     path('manga/<str:name>', views.manga_details, name='manga_details'),
     path('auteurs', views.auteurs_list, name='auteurs_list'),
     path('auteur/<str:name>', views.auteur_details, name='auteur_details'),
